93148e5902624804488046310d7ec7d6ff7beb95
Manuel <133500396+xxmanu0103@users.noreply.github.com>
2023-05-13 20:32:55 +0200

    add types

--- src/handler/requestHandler.ts
+++ src/handler/requestHandler.ts
@@ -1,14 +1,34 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { IRequestHandlerOptions, DebugLevel } from "./types";
 
+/**
+ * A class representing a request handler that sends HTTP requests using Axios.
+ */
 export class RequestHandler {
+  /**
+   * The Axios client instance used to make HTTP requests.
+   *
+   * @private
+   */
   private readonly client: AxiosInstance;
+
+  /**
+   * Object containing request and response interceptors.
+   *
+   * @private
+   */
   private readonly interceptors = {
     request: null as any,
     response: null as any,
   };
 
+  /**
+   * Creates a new `RequestHandler`.
+   *
+   * @param options - An object containing configuration options for the request handler.
+   */
   constructor(private readonly options: IRequestHandlerOptions) {
+    // Set default options for the Axios client.
     const defaultOptions: AxiosRequestConfig = {
       headers: this.options.headers ?? {},
       params: this.options.queryParams ?? {},
@@ -20,14 +40,17 @@ export class RequestHandler {
       validateStatus: this.options.validateStatus,
     };
 
+    // If an API key is provided, add it to the Authorization header.
     if (this.options.apiKey) {
       defaultOptions.headers![
         "Authorization"
       ] = `Bearer ${this.options.apiKey}`;
     }
 
+    // Create the Axios client instance.
     this.client = axios.create({ ...defaultOptions });
 
+    // Add request interceptor to log debug information.
     this.interceptors.request = this.client.interceptors.request.use(
       (config) => {
         if (
@@ -41,6 +64,7 @@ export class RequestHandler {
       }
     );
 
+    // Add response interceptor to log debug information.
     this.interceptors.response = this.client.interceptors.response.use(
       (response) => {
         if (this.options.debug && this.options.debug >= DebugLevel.MEDIUM) {
@@ -60,6 +84,11 @@ export class RequestHandler {
     );
   }
 
+  /**
+   * Sends an HTTP request using Axios.
+   *
+   * @returns A Promise that resolves with the HTTP response.
+   */
   public async sendRequest<T = any>(): Promise<AxiosResponse<T>> {
     try {
       const response = await this.client.request<T>({
@@ -91,6 +120,9 @@ export class RequestHandler {
     }
   }
 
+  /**
+   * Removes all request and response interceptors added to the Axios client instance.
+   */
   public removeInterceptors(): void {
     try {
       this.client.interceptors.request.eject(this.interceptors.request);
